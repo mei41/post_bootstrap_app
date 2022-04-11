ac74d747ce49bedf14d2fcc649310262043d2f5f
{mei41} <{oniku.1200tax@gmail.com}>
2022-04-11 15:52:23 +0900

    新規投稿機能を実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,17 @@
 class PostsController < ApplicationController
   def index
   end
-
+  
   def show
   end
-
+  
   def new
+    @post = Post.new
   end
 
   def create
+    post = Post.create!(post_params)
+    redirect_to post
   end
 
   def edit
@@ -18,5 +21,10 @@ class PostsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+  def post_params
+    params.reqire(:post).permit(:title, :content)
   end
 end
